958603b92496744ef8d206c4726954a0e10b8dbb
Anjan Roy <anjanroy@yandex.com>
2019-07-23 22:03:45 +0530

    added working express server, ready to deploy

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const express = require('express');
+const http = require('http');
+const app = express();
+
+app.get('/', (req, res) => {
+  console.log(`GET / ${req.ip} ${Date().toString()}`);
+  res.status(200).contentType('html').sendFile(
+      './pages/index.html', {root : __dirname}, (err) => {
+        if (err !== undefined && err !== null)
+          res.send('Something went wrong').end();
+      });
+});
+
+app.get('/index.css', (req, res) => {
+  console.log(`GET /index.css ${req.ip} ${Date().toString()}`);
+  res.status(200).sendFile('./styles/index.css', {root : __dirname},
+                           (err) => { res.end(); });
+});
+
+app.get('/myResume.pdf', (req, res) => {
+  console.log(`GET /myResume.pdf ${req.ip} ${Date().toString()}`);
+  res.status(200).sendFile('./cv/myResume.pdf', {root : __dirname},
+                           (err) => { res.end(); });
+});
+
+http.createServer(app).listen(80, '13.127.52.44',
+                              () => {console.log('[+]Server started')});
