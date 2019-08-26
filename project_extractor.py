3e9206f1d772643e6ba180cdf46b61fdde2abbc4
Anjan Roy <anjanroy@yandex.com>
2019-08-26 23:20:52 +0530

    updated git scrapper script

--- project_extractor.py
+++ project_extractor.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
+from typing import Dict, Any
 from requests import get
 from json import dump
 from os.path import dirname, abspath, join
 from urllib.parse import urljoin
 from dateutil.parser import parse
+from itertools import chain
+from functools import reduce
 try:
     from bs4 import BeautifulSoup
 except ImportError as e:
@@ -21,33 +24,85 @@ except ImportError as e:
 	finally written into a target_file, after serializing into JSON
 '''
 
-def extractIt(dataObject, targetClass: str = 'col-10 col-lg-9 d-inline-block', url: str = 'https://github.com/itzmeanjan'):
+
+def extractIt(dataObject, username: str, targetClass: str = 'col-10 col-lg-9 d-inline-block') -> Dict[str, Any]:
+    def __extractFromThisPage__():
+        def __findLicense(element):
+            try:
+                return element.findAll('span', {'class': 'mr-3'})[1].text.strip()
+            except Exception:
+                return ''
+
+        def __findForkCount__(element):
+            try:
+                return reduce(lambda acc, cur: acc + int(cur.text.strip()), filter(lambda e: e.get(
+                    'href').endswith('stargazers'), element), 0)
+            except Exception as e:
+                return 0
+
+        def __findStargazerCount__(element):
+            try:
+                return reduce(lambda acc, cur: acc + int(cur.text.strip()), filter(lambda e: e.get(
+                    'href').endswith('members'), element), 0)
+            except Exception:
+                return 0
+
+        return map(lambda e: (e.findChild('div', {'class': 'd-inline-block mb-1'}).h3.a.text.strip(), {
+            'url': urljoin('https://github.com/{}'.format(username), e.findChild('div', {'class': 'd-inline-block mb-1'}).h3.a.get('href')),
+            'description': e.find('p', {'class': 'col-9 d-inline-block text-gray mb-2 pr-4'}).text.strip(),
+            'lang': e.findChild('div', {'class': 'f6 text-gray mt-2'}).findChild('span', {'itemprop': 'programmingLanguage'}).text.strip(),
+            'langColor': e.findChild('div', {'class': 'f6 text-gray mt-2'}).findChild('span', {'class': 'ml-0 mr-3'}).findChild('span', {'class': 'repo-language-color'}).get('style').split(':')[1].strip(),
+            'fork': __findForkCount__(e.findChild('div', {'class': 'f6 text-gray mt-2'}).findAll('a', {'class': 'muted-link mr-3'})),
+            'star': __findStargazerCount__(e.findChild('div', {'class': 'f6 text-gray mt-2'}).findAll('a', {'class': 'muted-link mr-3'})),
+            'updated': parse(e.findChild('div', {'class': 'f6 text-gray mt-2'}).findChild('relative-time').get('datetime')).timestamp(),
+            'license': __findLicense(e.findChild('div', {'class': 'f6 text-gray mt-2'}))}), dataObject.findAll('div', {'class': targetClass}))
+
     try:
-        return map(lambda e: (e.findChild('div', {'class': 'd-inline-block mb-1'}).h3.a.text.strip(), {'url': urljoin('https://github.com/itzmeanjan', e.findChild('div', {'class': 'd-inline-block mb-1'}).h3.a.get('href')), 'description': e.find('p', {'class': 'col-9 d-inline-block text-gray mb-2 pr-4'}).text.strip(), 'lang': e.findChild('div', {'class': 'f6 text-gray mt-2'}).findChild('span', {'itemprop': 'programmingLanguage'}).text.strip(), 'langColor': e.findChild('div', {'class': 'f6 text-gray mt-2'}).findChild('span', {'class': 'ml-0 mr-3'}).findChild('span', {'class': 'repo-language-color'}).get('style').split(':')[1].strip(), 'updated': parse(e.findChild('div', {'class': 'f6 text-gray mt-2'}).findChild('relative-time').get('datetime')).timestamp()}), dataObject.findAll('div', {'class': targetClass}))
-    except Exception:
+        paginatedObject = dataObject.findChild(
+            'div', {'class': 'paginate-container'})
+        if(paginatedObject):
+            linksToFollow = paginatedObject.div.findAll('a')
+            if(len(linksToFollow) == 2):
+                return chain(__extractFromThisPage__(), extractIt(BeautifulSoup(fetchIt(
+                    linksToFollow[1].get('href')), features='html.parser'), username=username))
+            else:
+                if(linksToFollow[0].text.strip().lower() == 'next'):
+                    return chain(__extractFromThisPage__(), extractIt(BeautifulSoup(fetchIt(
+                        linksToFollow[0].get('href')), features='html.parser'), username=username))
+                else:
+                    return __extractFromThisPage__()
+        else:
+            return __extractFromThisPage__()
+    except Exception as e:
         return None
+
 
 '''
 	fetches content of target webpage, which will be parsed using some `html` parser
 '''
 
-def fetchIt(url: str = 'https://github.com/itzmeanjan?tab=repositories') -> str:
+
+def fetchIt(url: str) -> str:
     try:
         resp = get(url)
         return resp.content if(resp.status_code is 200) else None
     except Exception:
         return None
 
+
 '''
 	main entry point of script, calls required methods in
 	proper order to finally write extracted data into target_file as JSON string
 '''
 
-def app(target_file: str = abspath(join(dirname(__file__), 'data/projects.json'))) -> bool:
+
+def app(username: str, target_file: str = abspath(join(dirname(__file__), 'data/projects.json'))) -> bool:
     try:
+
         with open(target_file, 'w') as fd:
-            dump(dict(extractIt(BeautifulSoup(fetchIt(), features='html.parser'))),
-                 fd, ensure_ascii=False, indent=4)
+            dump(dict(extractIt(BeautifulSoup(fetchIt('https://github.com/{}?tab=repositories'.format(
+                username)), features='html.parser'), username=username)),
+                fd, ensure_ascii=False, indent=4)
         return True
     except Exception:
         return False
@@ -55,7 +110,7 @@ def app(target_file: str = abspath(join(dirname(__file__), 'data/projects.json')
 
 if __name__ == '__main__':
     try:
-        print('success' if app() else 'failure')
+        print('success' if app('itzmeanjan') else 'failure')
     except KeyboardInterrupt:
         print('\n[!]Terminated')
     finally:
